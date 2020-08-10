424642a1dd98c480a8c88a352f7947133e56d47f
Tim Cinel <email@timcinel.com>
2020-08-11 02:13:58 +1000

    Seems to need more headers now... Added them.

--- auswoo.py
+++ auswoo.py
@@ -18,10 +18,19 @@ def log_in(session, username, password):
     }
 
     headers = {
-        "Accept": "application/json",
+        "Accept": "application/json, text/plain, */*",
         "Content-Type": "application/json",
         "AP_APP_ID": "MYPOST",
         "Origin": "https://auspost.com.au",
+        "Referer": "https://auspost.com.au/auth/login?caller=ACCOUNT_GLOBAL_HEADER&product=MYPOST_CONSUMER&channel=WEB",
+        "Connection": "keep-alive",
+        "AP_APP_ID": "MYPOST",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+        "Content-Type": "application/json",
+        "Sec-Fetch-Site": "same-site",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Dest": "empty",
+        "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
     }
 
     return session.post('https://digitalapi.auspost.com.au/cssoapi/v2/session', json=json, headers=headers)
